488689fa74a6253e24b0ed351831f480b4b26805
xudegyuang1 <rickyzzzz@163.com>
2024-06-19 17:56:50 +0800

    demo1

---
+++ src/main/java/com/miaoshaproject/demo1.java
@@ -0,0 +1,14 @@
+package com.miaoshaproject;
+
+public class demo1 {
+    public static void main(String[] args) {
+        System.out.println("hello world-master1");
+        System.out.println("hello world-master2");
+        System.out.println("hello world-master3");
+        System.out.println("hello world");
+        System.out.println("hello world1");
+        System.out.println("hello world2");
+        System.out.println("hello world3");
+        System.out.println("hello world");
+    }
+}
