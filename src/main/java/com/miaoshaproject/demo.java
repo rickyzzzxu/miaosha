9ed74e38e35ca336c5180796b9522d2d9b92b90e
Ricky <98084369+rickyzzzxu@users.noreply.github.com>
2024-06-19 17:16:30 +0800

    Update demo.java

--- src/main/java/com/miaoshaproject/demo.java
+++ src/main/java/com/miaoshaproject/demo.java
@@ -2,6 +2,7 @@ package com.miaoshaproject;
 
 public class demo {
     public static void main(String[] args) {
+        System.out.println("hello world-master1");
         System.out.println("hello world");
         System.out.println("hello world");
     }
