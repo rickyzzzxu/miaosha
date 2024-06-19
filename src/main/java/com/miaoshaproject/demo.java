aa88b4c991321c69caf7fdd60a852c95742e0cc8
xudegyuang1 <rickyzzzz@163.com>
2024-06-19 17:39:40 +0800

    demo

--- src/main/java/com/miaoshaproject/demo.java
+++ src/main/java/com/miaoshaproject/demo.java
@@ -8,6 +8,7 @@ public class demo {
         System.out.println("hello world");
         System.out.println("hello world1");
         System.out.println("hello world2");
+        System.out.println("hello world3");
         System.out.println("hello world");
     }
 }
